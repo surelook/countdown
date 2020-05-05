f8a31e5cb7ad6e1d8a48cf446fd30a8a4c83ff8f
Marc Carroll <marc@deveire.com>
2020-05-05 21:57:11 +0100

    const correction

--- src/analytics.js
+++ src/analytics.js
@@ -1,14 +1,14 @@
 import Analytics from 'analytics'
 import googleAnalytics from '@analytics/google-analytics'
  
-const trackingIDd = process.env.GA_TRACKING_ID
+const trackingId = process.env.GA_TRACKING_ID
 
 if (trackingId) {
     const analytics = Analytics({
         app: 'countdown',
         plugins: [
           googleAnalytics({
-            trackingId: trackingIDd
+            trackingId: trackingId
           })
         ]
       })
